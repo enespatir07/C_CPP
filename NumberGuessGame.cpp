3f0bcd6e67ce8a872d0cedb0bce02f22ad52561b
enes <42852889+enespatir07@users.noreply.github.com>
2022-07-26 01:05:54 +0200

    Update NumberGuessGame.cpp

--- NumberGuessGame.cpp
+++ NumberGuessGame.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <time.h>
 int main() {
-    int input, rand_number, lives = 5;//number of attemps as "lives"
+    int input, rand_number, lives = 5;
     srand(time(0));
-    rand_number = rand() % 10 + 1;//generating random number
+    rand_number = rand() % 10 + 1;
     while (lives > 0) {
         std::cout << "Enter a number (1-10): ";
         std::cin >> input;
-        if (rand_number == input) {//win condition
+        if (rand_number == input) {
             std::cout << "You won" << std::endl;
             return 0;
-        } else if (input < rand_number) {
+        } else if (input < rand_number)
             std::cout << input << " is smaller" << std::endl;
-        } else {
+        else
             std::cout << input << " is greater" << std::endl;
-        }
         lives--;
         if (lives != 0)
             std::cout << "Remaining lives: " << lives << std::endl;
     }
-    std::cout << "you lost" << std::endl;//lost situation
+    std::cout << "you lost" << std::endl;
 }
