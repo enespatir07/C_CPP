42cfd2b06c02920f75583550ea8f922d4eb3e5c1
enes <42852889+enespatir07@users.noreply.github.com>
2022-08-21 22:35:09 +0200

    Update SelectionSort.cpp

--- Algortihm Problems/SelectionSort.cpp
+++ Algortihm Problems/SelectionSort.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-class Sort {
+
+class SelectionSort {
 private:
     static void swap(int *x, int *y) {
         int temp = *x;
         *x = *y;
         *y = temp;
     }
+
 public:
     static void selectionSort(int *arr, int size) {
 
         for (int i = 0; i < size - 1; i++) {
-            int minIndex = i;
+            int minIndex = i;//variable to store the index of smallest number
             for (int j = i + 1; j < size; j++) {
                 if (arr[minIndex] > arr[j]) {
                     minIndex = j;
@@ -19,24 +21,4 @@ public:
             swap(&arr[minIndex], &arr[i]);
         }
     }
-    static void printArray(int *arr, int size) {
-        for (int i = 0; i < size; i++)
-            std::cout << arr[i] << " ";
-    }
 };
-
-int main() {
-    int size;
-    std::cout << "Enter the size of the array: ";
-    std::cin >> size;
-    int *arr = new int(size);
-    for (int i = 0; i < size; i++) {
-        std::cout << "Element-" << i + 1 << ": ";
-        std::cin >> arr[i];
-    }
-    std::cout << "Original array: ";
-    Sort::printArray(arr, size);
-    Sort::selectionSort(arr, size);
-    std::cout << std::endl <<"Sorted array: ";
-    Sort::printArray(arr, size);
-}
